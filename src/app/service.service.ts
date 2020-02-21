74c8e38627f1519734b98f38c7afdd5863f1d4a6
grishma <grishmapatil06@gmail.com>
2020-02-21 21:54:48 +0530

    updated the cart

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { stringify } from 'querystring';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+ 
 
-  private urlProductList = "http://localhost:8989/OnlineShopping/productList";
+ // private urlProductList = "http://localhost:8989/OnlineShopping/productList";
   // private urlProductList = "http://192.168.14.86:8989/OnlineShoppingRest/productList";
-  //private urlProductList = "http://localhost:8989/Product_Details/productList";
-  // private urlProductList = "http://192.168.14.86:8989/OnlineShoppingRest/productList";
+     private urlProductList = "http://192.168.14.87:8989/OnlineShopping/productList";
+//   private urlProductList = "http://192.168.14.87:8989/OnlineShoppingRest/productList";
 
-  private urlUserList = "http://localhost:8989/OnlineShopping/UserList";
+  private urlUserList = "http://192.168.14.87:8989/OnlineShopping/UserList";
+  private urlProduct = "http://localhost:8989/OnlineShopping/fetchProduct";
   constructor(private http: HttpClient) { }
 
   getProductList(){
@@ -21,4 +24,10 @@ export class ServiceService {
   getUserList(){
     return this.http.get(this.urlUserList);
   }
+  
+  getProduct(productId:number){
+    return this.http.put(this.urlProduct,productId);
+
+  }
+
 }
